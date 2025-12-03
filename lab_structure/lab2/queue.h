750f65ef612922bde544754182cf53e31684a69d
gislowloves <margo.dron@bk.ru>
2025-12-03 21:51:52 +0300

    Update queue.h

---
+++ lab_structure/lab2/queue.h
@@ -0,0 +1,21 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "stack.h"
+#include <cstdint>
+
+template <typename T>
+class Queue {
+private:
+    Stack<T> stik1;
+    Stack<T> stik2;
+    void transfer();
+public:
+    Queue(std::int64_t capacity);
+    void enqueue(T x);
+    T dequeue();
+    bool isEmpty();
+    std::int64_t size();
+};
+
+#endif
