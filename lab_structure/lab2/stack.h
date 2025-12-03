46741ae8bd5bfa4cc5eaadc0e0d46120943aa699
gislowloves <margo.dron@bk.ru>
2025-12-03 21:52:55 +0300

    Update stack.h

---
+++ lab_structure/lab2/stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "DynamicArray.h"
+#include <cstdint>
+
+template <typename T>
+class Stack {
+private:
+    DynamicArray<T> data;
+    std::int64_t mSize;
+public:
+    Stack(std::int64_t mSize);
+    void push(T x);
+    T top();
+    T pop();
+    bool isEmpty();
+    void printStack();
+    std::int64_t stSize();
+};
+
+#endif
